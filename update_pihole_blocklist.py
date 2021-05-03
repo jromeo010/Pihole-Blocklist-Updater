ba713fae9a2a82a20ebaeacba6be60e7f0574462
jromeo010 <54372021+jromeo010@users.noreply.github.com>
2021-05-03 15:59:20 -0400

    Add files via upload

---
+++ update_pihole_blocklist.py
@@ -0,0 +1,46 @@
+#run as Sudo
+import requests, csv, sqlite3, os, logging
+
+def main():
+
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s -  %(levelname)s -  %(message)s',filename='pihole_update.log')
+    block_list = requests.get('https://v.firebog.net/hosts/csv.txt')
+    SUCCESS_FLAG = True
+    if block_list.status_code != 200:
+        logging.error('Couldnt connect to URL -- exiting scirpt')
+        quit()
+
+    COLUMNS = ['category','ticktype','source repo','description','source URL']
+    records = csv.DictReader(block_list.text.splitlines(),fieldnames=COLUMNS)
+
+    try:
+        con = sqlite3.connect('/etc/pihole/gravity.db')
+        cur = con.cursor()
+    except:
+        logging.error('Error connecting to sql lite - exiting script')
+        SUCCESS_FLAG = False
+        quit()
+
+
+
+    for record in records:
+        if  record['ticktype'] == 'tick':
+            BLOCK_LIST_URL = record['source URL']
+            INSERT_STATEMENT = "INSERT OR IGNORE INTO adlist (Address) VALUES ('{}');".format(BLOCK_LIST_URL)
+            try:
+                cur.execute(INSERT_STATEMENT)
+                con.commit()
+            except:
+                logging.error('Error on insert - exiting script')
+                SUCCESS_FLAG = False
+                break
+    con.close()
+
+    os.system('pihole -g')
+
+    if SUCCESS_FLAG == True:
+        logging.info('Successfully Updated Block List')
+
+
+if __name__ == "__main__":
+    main()
